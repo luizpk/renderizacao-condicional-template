9bc7bb1b6db7aa276a4572f56724b9fd822f741e
luizpk <luizpkpinheiro@gmail.com>
2022-12-26 15:39:41 -0300

    Resolvendo exercício utilizando if/else e switch case

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import { useState } from "react";
 
 const GlobalStyled = createGlobalStyle`
   *{
@@ -15,13 +16,51 @@ const MainContainer = styled.main`
 
 function App() {
 
-  return (
-    <MainContainer >
+  const [tela, setTela] = useState(1);
+
+  /* Utilizando if/else*/
+
+  // if (tela===1){
+
+  //   return (
+  //     <MainContainer >
+  //       <GlobalStyled />
+  //       <TelaLogin />
+  //       </MainContainer>
+  //   );
+  // } else {
+  //   return (
+  //     <MainContainer >
+  //       <GlobalStyled />
+  //       <TelaCadastro />
+  //     </MainContainer>
+  //   );
+  // }
+
+/* utilizando switch case*/
+
+  switch (tela) {
+    case 1:
+      return (
+      <MainContainer >
       <GlobalStyled />
       <TelaLogin />
+      </MainContainer>);
+    case 2:
+      return (
+      <MainContainer >
+      <GlobalStyled />
       <TelaCadastro />
-    </MainContainer>
-  );
+      </MainContainer>
+      );
+    
+    default:
+      console.log("Pagina não encontrada."); 
+
+  }
+
+
+  
 }
 
 export default App;
